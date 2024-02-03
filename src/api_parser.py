9d6acd0ed473c4915147da2f7ace6b45b244a429
QueCS <73703089+QueCS@users.noreply.github.com>
2024-02-03 22:12:15 +0100

    fixed filemode and added more functions

--- src/api_parser.py
+++ src/api_parser.py
@@ -10,6 +10,7 @@ log_lvl = logging.INFO
 
 logging.basicConfig(
     filename=f'{log_dir}/api_parsing.log',
+    filemode='a',
     format='%(asctime)s %(levelname)s %(message)s',
     level=log_lvl
 )
@@ -23,14 +24,23 @@ def return_highscore_api(server, community, category, type):
     url = f'https://s{server}-{community}.ogame.gameforge.com/api/highscore.xml?category={category}&type={type}'
     response = requests.get(url, timeout=1, allow_redirects=False)
     if response.status_code == 200:
-        logging.info(f'Fetched {url}')
+        logging.info(f'Fetched api: {url}')
         return response.text
     logging.critical(f'When calling return_highscore_api, unable to fetch data, status code {response.status_code}')
     sys.exit(1)
 
-def return_timestamp(xml):
-    timestamp = re.search(r'timestamp="(\d+)"', xml)
-    logging.info('Fetched timestamp')
+def return_player_api(server, community, player_id):
+    url = f'https://s{server}-{community}.ogame.gameforge.com/api/playerData.xml?id={player_id}'
+    response = requests.get(url, timeout=1, allow_redirects=False)
+    if response.status_code == 200:
+        logging.info(f'Fetched api: {url}')
+        return response.text
+    logging.critical(f'When calling return_player_api, unable to fetch data, status code {response.status_code}')
+    sys.exit(1)
+
+def return_api_timestamp(api_xml):
+    timestamp = re.findall(r'timestamp="(\d+)"', api_xml)[0]
+    logging.info(f'Fetched timestamp: {timestamp}')
     return timestamp
 
 
