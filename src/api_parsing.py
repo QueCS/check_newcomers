49baa371d07b09f1d8e8356961cda680f021d69b
QueCS <73703089+QueCS@users.noreply.github.com>
2024-02-03 23:10:32 +0100

    additional logging and automatic linting (ruff) when saving

--- src/api_parsing.py
+++ src/api_parsing.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     filename=f'{log_dir}/api_parsing.log',
     filemode='a',
     format='%(asctime)s %(levelname)s %(message)s',
-    level=log_lvl
+    level=log_lvl,
 )
 
 
@@ -24,8 +24,10 @@ def is_xml(string):
     try:
         et.fromstring(string)
     except et.ParseError:
+        logging.error(f'When calling is_xml(), "{string[0:50]}" (truncated if above 50 caracters) is not a valid xml')
         return False
     return True
+
 
 def return_highscore_api(server, community, category, type):
     url = f'https://s{server}-{community}.ogame.gameforge.com/api/highscore.xml?category={category}&type={type}'
@@ -33,8 +35,9 @@ def return_highscore_api(server, community, category, type):
     if response.status_code == 200:
         logging.info(f'Fetched api: {url}')
         return response.text
-    logging.critical(f'When calling return_highscore_api, unable to fetch data, status code {response.status_code}')
+    logging.critical(f'When calling return_highscore_api(), unable to fetch data, status code {response.status_code}')
     sys.exit(1)
+
 
 def return_player_api(server, community, player_id):
     url = f'https://s{server}-{community}.ogame.gameforge.com/api/playerData.xml?id={player_id}'
@@ -42,26 +45,30 @@ def return_player_api(server, community, player_id):
     if response.status_code == 200:
         logging.info(f'Fetched api: {url}')
         return response.text
-    logging.critical(f'When calling return_player_api, unable to fetch data, status code {response.status_code}')
+    logging.critical(f'When calling return_player_api(), unable to fetch data, status code {response.status_code}')
     sys.exit(1)
+
 
 def return_player_name(player_api_xml):
     if is_xml(player_api_xml):
         return re.findall(r'name="([^"]+)"', player_api_xml)[0]
-    logging.critical('When calling return_player_name, no valid xml to parse')
+    logging.critical('When calling return_player_name(), no valid xml to parse')
     sys.exit(1)
+
 
 def return_player_coords(player_api_xml):
     if is_xml(player_api_xml):
         return re.findall(r'coords="([^"]+)"', player_api_xml)
-    logging.critical('When calling return_player_coords, no valid xml to parse')
+    logging.critical('When calling return_player_coords(), no valid xml to parse')
     sys.exit(1)
+
 
 def return_player_home_planet_coords(player_api_xml):
     if is_xml(player_api_xml):
         return return_player_coords(player_api_xml)[0]
-    logging.critical('When calling return_player_coords, no valid xml to parse')
+    logging.critical('When calling return_player_home_planet_coords(), no valid xml to parse')
     sys.exit(1)
+
 
 def return_api_timestamp(api_xml):
     timestamp = re.findall(r'timestamp="(\d+)"', api_xml)[0]
