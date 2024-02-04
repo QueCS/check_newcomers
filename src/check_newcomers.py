bca2a58da840a8b77b6c998294da72fe9b1b135e
QueCS <73703089+QueCS@users.noreply.github.com>
2024-02-04 18:41:38 +0100

    fixed mix-up between current players and new players

--- src/check_newcomers.py
+++ src/check_newcomers.py
@@ -17,19 +17,19 @@ def main():
 
     ts_match, old_timestamp, new_timestamp = timestamps_match(military_highscore_api_xml)
     if ts_match:
-        logging.info(f'timestamps_match(), {new_timestamp} = {old_timestamp}, nothing to update, exiting...\n')
+        logging.info(f'timestamps_match(), {new_timestamp} = {old_timestamp}, nothing to update, exiting\n')
         sys.exit(0)
-    logging.info(f'timestamps_match(), {new_timestamp} != {old_timestamp}, carrying on...')
+    logging.info(f'timestamps_match(), {new_timestamp} != {old_timestamp}')
 
     update_timestamp_file(new_timestamp)
 
-    pl_match, new_players = players_match(military_highscore_api_xml)
+    pl_match, current_players, new_players = players_match(military_highscore_api_xml)
     if pl_match:
-        logging.info('players_match(), no new players detected, nothing to update, exiting...\n')
+        logging.info('players_match(), no new players detected, nothing to update, exiting\n')
         sys.exit(0)
-    logging.info(f'players_match(), new players detected: {new_players}, carrying on...')
+    logging.info(f'players_match(), new players detected: {new_players}')
 
-    update_players_file(new_players)
+    update_players_file(current_players)
 
 
 def timestamps_match(api_xml):
@@ -51,12 +51,13 @@ def update_timestamp_file(new_timestamp):
 
 def players_match(api_xml):
     if ap.is_xml(api_xml):
-        new_players = ap.return_player_ids_from_highscore_api(api_xml)
+        current_players = ap.return_player_ids_from_highscore_api(api_xml)
         with open(f'{data_dir}/{server}_{community}_players.json', 'r') as old_players_file:
             old_players = json.load(old_players_file)
-            if new_players == old_players:
-                return True, new_players
-            return False, new_players
+            new_players = [x for x in current_players if x not in set(old_players)]
+            if current_players == old_players:
+                return True, current_players, new_players
+            return False, current_players, new_players
 
 
 def update_players_file(new_players):
