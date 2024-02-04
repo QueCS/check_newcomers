2d8db175cd57369eb07e7337f3c1d6ab249f79e2
QueCS <73703089+QueCS@users.noreply.github.com>
2024-02-04 17:58:09 +0100

    logic to check for new players and update players file if need be

--- check_newcomers.py
+++ check_newcomers.py
@@ -21,18 +21,28 @@ logging.basicConfig(
 
 def main():
     military_highscore_api_xml = ap.return_highscore_api(server, community, 1, 3)
-    match, old_timestamp, new_timestamp = timestamps_match(military_highscore_api_xml)
-    if match:
-        logging.info(f'fetch_compare_update_timestamp(), timestamps match, {new_timestamp} = {old_timestamp}, nothing to update, exiting...')
+
+    ts_match, old_timestamp, new_timestamp = timestamps_match(military_highscore_api_xml)
+    if ts_match:
+        logging.info(f'timestamps_match(), timestamps match, {new_timestamp} = {old_timestamp}, nothing to update, exiting...')
         sys.exit(0)
-    logging.info(f'fetch_compare_update_timestamp(), timestamps do not match, {new_timestamp} != {old_timestamp}, carrying on...')
+    logging.info(f'timestamps_match(), timestamps do not match, {new_timestamp} != {old_timestamp}, carrying on...')
+
     update_timestamp_file(new_timestamp)
+
+    pl_match, new_players = players_match(military_highscore_api_xml)
+    if pl_match:
+        logging.info('players_match(), old and new player lists match, nothing to update, exiting...')
+        sys.exit(0)
+    logging.info(f'players_match(), new players detected, {new_players}, carrying on...')
+
+    update_players_file(new_players)
 
 
 def timestamps_match(api_xml):
     if ap.is_xml(api_xml):
         new_timestamp = ap.return_api_timestamp(api_xml)
-        with open('data/timestamp.json', 'r') as old_timestamp_file:
+        with open(f'data/{server}_{community}_timestamp.json', 'r') as old_timestamp_file:
             old_timestamp = json.load(old_timestamp_file)
             if new_timestamp == old_timestamp:
                 return True, old_timestamp, new_timestamp
@@ -44,6 +54,21 @@ def timestamps_match(api_xml):
 def update_timestamp_file(new_timestamp):
     with open(f'{data_dir}/timestamp.json', 'w') as timestamp_file:
         json.dump(new_timestamp, timestamp_file)
+
+
+def players_match(api_xml):
+    if ap.is_xml(api_xml):
+        new_players = ap.return_player_ids_from_highscore_api(api_xml)
+        with open(f'data/{server}_{community}_players.json', 'r') as old_players_file:
+            old_players = json.load(old_players_file)
+            if new_players == old_players:
+                return True
+            return False, new_players
+
+
+def update_players_file(new_players):
+    with open(f'{data_dir}/players.json', 'w') as players_file:
+        json.dump(new_players, players_file)
 
 
 if __name__ == '__main__':
