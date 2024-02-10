a36415be61836bf084fda85355ddf8d941786afb
QueCS <73703089+QueCS@users.noreply.github.com>
2024-02-10 12:28:55 +0100

    function calls failure handling

--- src/check_newcomers.py
+++ src/check_newcomers.py
@@ -37,11 +37,16 @@ def main():
 
     Returns if no new player arrived:
         bool: False
+
+    Returns if either get_highscore_api() or get_player_ids() calls were unsuccessful:
+        str: Stating were the script failed.
     """
 
     logging.info('Starting up !')
 
     highscore_api = ap.get_highscore_api(server, community, '1', '3')
+    if highscore_api is None:
+        return '```\nError: highscore_api is None\n```'
 
     # Compare old and new timestamp to determine whether the API was updated or not
     with open(f'{data_dir}/{server}_{community}_timestamp.json', 'r') as timestamp_file:
@@ -55,7 +60,11 @@ def main():
     # Compare old and current player lists checking for new players
     with open(f'{data_dir}/{server}_{community}_players.json', 'r') as players_file:
         old_players = json.load(players_file)
+
     current_players = ap.get_player_ids(highscore_api)
+    if current_players is None:
+        return '```\nError: current_players is None\n```'
+
     new_players = [x for x in current_players if x not in old_players]
     if len(new_players) == 0:
         logging.info(f'No new players detected, updating {data_dir}/{server}_{community}_timestamp.json and exiting !\n')
@@ -67,7 +76,7 @@ def main():
 
     # Set up payload string
     update_datetime = datetime.datetime.fromtimestamp(new_ts)
-    new_players_str = f'```\n{update_datetime}\n'
+    payload = f'```\n{update_datetime}\n'
 
     # Loop through new players fetching data
     for player_id in new_players:
@@ -87,7 +96,7 @@ def main():
         military_ships_str = (f'{player_ship_count:,}').replace(',', '.')
 
         # Append new data to payload string
-        new_players_str += f'\n{player_name} ({player_id}, {player_home}) {military_points_str} ({military_ships_str})\n'
+        payload += f'\n{player_name} ({player_id}, {player_home}) {military_points_str} ({military_ships_str})\n'
 
         # Update players.json with processed player
         logging.info(f'Adding entry to {data_dir}/{server}_{community}_players.json')
@@ -99,7 +108,7 @@ def main():
         time.sleep(0.5)
 
     # Finalize the payload string
-    new_players_str += '```'
+    payload += '```'
 
     # Update timestamps.json
     logging.info(f'Updating {data_dir}/{server}_{community}_timestamp.json')
@@ -108,7 +117,7 @@ def main():
 
     logging.info('Done !\n')
 
-    return new_players_str
+    return payload
 
 
 if __name__ == '__main__':
